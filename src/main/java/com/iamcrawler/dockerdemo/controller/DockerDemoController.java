38c5c3b719b29d1a02d6c082682f610b569e9e0e
liuliang <liuliang@meifute.com>
2019-10-10 10:36:25 +0800

    first commit

---
+++ src/main/java/com/iamcrawler/dockerdemo/controller/DockerDemoController.java
@@ -0,0 +1,20 @@
+package com.iamcrawler.dockerdemo.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * create by liuliang
+ * on 2019-10-10  10:34
+ */
+@RestController
+@RequestMapping("/docker")
+public class DockerDemoController {
+
+    @GetMapping("/hello")
+    public ResponseEntity demo(){
+        return ResponseEntity.ok("hello docker");
+    }
+}
