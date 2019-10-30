6cee717e0c7855a61c4ffab6c4b8f6ec7971e007
liuliang <liuliang@meifute.com>
2019-10-30 10:35:52 +0800

    提交代码

--- src/main/java/com/iamcrawler/dockerdemo/controller/DockerDemoController.java
+++ src/main/java/com/iamcrawler/dockerdemo/controller/DockerDemoController.java
@@ -1,9 +1,16 @@
 package com.iamcrawler.dockerdemo.controller;
 
+import com.iamcrawler.dockerdemo.entity.MyUser;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
 
 /**
  * create by liuliang
@@ -29,5 +36,38 @@ public class DockerDemoController {
     @GetMapping("/test1")
     public ResponseEntity mysqlDemo1(){
         return ResponseEntity.ok("test1");
+    }
+
+    public static void main(String[] args) {
+
+//        List<MyUser> list = new ArrayList<>();
+//        MyUser myUser  = new MyUser();
+//        myUser.setId("1");
+//        myUser.setName("ll");
+//        list.add(myUser);
+        List<MyUser> list = new ArrayList<>();
+        Optional.ofNullable(list).orElseGet(
+                ()->{
+
+
+                    return new ArrayList<>();
+
+
+                }).stream().forEach(
+                a->{
+
+                    a.setName("123");
+
+
+                }
+        );
+
+        Executors.newFixedThreadPool(3);
+
+        System.out.println(list);
+
+
+//        Optional<MyUser>
+
     }
 }
---
+++ src/main/java/com/iamcrawler/dockerdemo/entity/MyUser.java
@@ -0,0 +1,13 @@
+package com.iamcrawler.dockerdemo.entity;
+
+import lombok.Data;
+
+/**
+ * create by liuliang
+ * on 2019-10-25  17:49
+ */
+@Data
+public class MyUser {
+    private String id;
+    private String name;
+}
